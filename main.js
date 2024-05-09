98da391f67e62522e0dd42a952405bdb4a4916b7
MattiaUsai <usai.mattia.05.04.2003@gmail.com>
2024-05-09 16:32:13 +0200

    PROVE

---
+++ main.js
@@ -0,0 +1,30 @@
+
+
+// //HO CREATO UNA FUNZIONÈCHE AL CLICCARE DEL BOTTONE MI DIA UN RETURN CHE INSERISCO IO COME PARAMETRO
+// function buttonclick (id,variables,result,value){
+    
+//      variables = document.getElementById(`bt${id}`);
+//     variables.addEventListener("click" , function(){ 
+//      console.log(result)
+//     value = value + result;
+    
+//     return result;
+//     })
+    
+
+  
+   
+// }
+
+// let value = 0;
+// buttonclick ('num1', 'btnumber1',1,value);
+// buttonclick ('num2', 'btnumber2',2,value);
+// buttonclick ('num3', 'btnumber3',3,value);
+// buttonclick ('num4', 'btnumber4',4,value);
+// buttonclick ('num5', 'btnumber5',5,value);
+// buttonclick ('num6', 'btnumber6',6,value);
+// buttonclick ('num7', 'btnumber7',7,value);
+// buttonclick ('num8', 'btnumber8',8,value);
+// buttonclick ('num9', 'btnumber9',9,value);
+
+// console.log (value);
