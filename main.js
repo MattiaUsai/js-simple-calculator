ce4292da90ceb06395856caa4b81796dffc062fd
MattiaUsai <usai.mattia.05.04.2003@gmail.com>
2024-05-09 18:19:18 +0200

    added swich

--- main.js
+++ main.js
@@ -22,14 +22,16 @@ buttonclick ('num6', 'btnumber6',6);
 buttonclick ('num7', 'btnumber7',7);
 buttonclick ('num8', 'btnumber8',8);
 buttonclick ('num9', 'btnumber9',9);
+buttonclick ('num0', 'btnumber0',0);
+buttonclick ('functionclear', 'btfunctionclear',"canc");
+buttonclick ('functionplus', 'btfunctionplus','+');
+buttonclick ('functionminus', 'btfunctionminus','-');
+buttonclick ('functiondivision', 'btfunctiondivision','/');
+buttonclick ('functionper', 'btfunctionper','*');
 
 // console.log (value);
 
-function buttonclickk (result){
-    console.log('ho cliccato il numero',result)
-    
 
-}
 let screen = document.getElementById('screen');
 
 
@@ -38,8 +40,46 @@ let screen = document.getElementById('screen');
 //     screen.innerHTML += 1;
 // })
 function buttonclickk (result){
+    switch (result){
+        case '+':
+            screen.innerHTML = '';
+            break;
+
+        case '-':
+            screen.innerHTML = '';
+            break;
+
+        case '*':
+            screen.innerHTML = '';
+            break;
+
+        case '/':
+            screen.innerHTML = '';
+            break;
+        case 1 :
+        case 2 :
+        case 3 :
+        case 4 :
+        case 5 :
+        case 6 :
+        case 7 :
+        case 8 :
+        case 9 :
+        case 0 :
+            
+
+        screen.innerHTML += result;
+        break;
+
+
+
+
+
+    }
+
+
+
     console.log('ho cliccato il numero',result)
-    screen.innerHTML += result
 
 }
 
