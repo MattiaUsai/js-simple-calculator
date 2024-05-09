4166eb5f1096089ab953f9f439b447c9d5661cd1
MattiaUsai <usai.mattia.05.04.2003@gmail.com>
2024-05-09 19:04:17 +0200

    added equal

--- main.js
+++ main.js
@@ -28,13 +28,16 @@ buttonclick ('functionplus', 'btfunctionplus','+');
 buttonclick ('functionminus', 'btfunctionminus','-');
 buttonclick ('functiondivision', 'btfunctiondivision','/');
 buttonclick ('functionper', 'btfunctionper','*');
+buttonclick ('functionequal', 'btfunctionequal','=');
 
 // console.log (value);
 
 
 let screen = document.getElementById('screen');
 let firstOperator = 0;
+let secondOperator = 0;
 let sign;
+let uguale = 0;
 
 // let num1 = document.getElementById('btnum1')
 // num1.addEventListener('click', function(){
@@ -42,38 +45,55 @@ let sign;
 // })
 function buttonclickk (result){
     switch (result){
+        //tasto cancella 
         case 'canc':
             screen.innerHTML = '';
-            firstOperator = screen.innerHTML;
+            firstOperator = 0;
+            secondOperator = 0;
+            result= 0;
             console.log (firstOperator);
 
             break;
+            //TASTO ADDIZIONE
         case '+':
-            sign = '+';
+            uguale = firstOperator + secondOperator;
             firstOperator = +(screen.innerHTML) ;
             screen.innerHTML = '';
             console.log (firstOperator);
 
             break;
-
+            //TASTO SOTTRAZIONE
         case '-':
-            sign = '-';
+            uguale = firstOperator - secondOperator;
             firstOperator = +(screen.innerHTML) ;
             screen.innerHTML = '';
             console.log (firstOperator);
             break;
-
+            //TASTO MOLTIPLICAZIONE
         case '*':
+            uguale = firstOperator * secondOperator;
             firstOperator = +(screen.innerHTML) ;
             screen.innerHTML = '';
             console.log (firstOperator);
             break;
-
+            //TASTO DIVISIONE
         case '/':
+            uguale = firstOperator / secondOperator;
             firstOperator = +(screen.innerHTML) ;
             screen.innerHTML = '';
             console.log (firstOperator);
             break;
+           // TASTO RISULTATO
+        case '=':
+            secondOperator = +(screen.innerHTML) ;
+            screen.innerHTML = '';
+            screen.innerHTML = uguale;
+            console.log (firstOperator,sign,secondOperator);
+            break
+
+
+
+        
         case 1 :
         case 2 :
         case 3 :
@@ -97,7 +117,7 @@ function buttonclickk (result){
 
 
 
-    console.log('ho cliccato il numero',result)
+    console.log(firstOperator)
 
 }
 
